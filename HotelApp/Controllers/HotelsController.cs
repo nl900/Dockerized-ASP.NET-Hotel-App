33de8facd8d479880b38d0ab6d25795e1a944c6e
NL <nl@192-168-1-104.tpgi.com.au>
2019-11-28 15:44:52 +1100

    crud operations service

---
+++ HotelApp/Controllers/HotelsController.cs
@@ -0,0 +1,74 @@
+﻿using HotelsApi.Models;
+using HotelsApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ListingsApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private readonly HotelService _hotelService;
+
+        public HotelsController(HotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Hotel>> Get() =>
+            _hotelService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetHotel")]
+        public ActionResult<Hotel> Get(string id)
+        {
+            var hotel = _hotelService.Get(id);
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return hotel;
+        }
+
+        [HttpPost]
+        public ActionResult<Hotel> Create(Hotel hotel)
+        {
+            _hotelService.Create(hotel);
+
+            return CreatedAtRoute("GetHotel", new { id = hotel.Id.ToString() }, hotel);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Hotel hotelIn)
+        {
+            var hotel = _hotelService.Get(id);
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            _hotelService.Update(id, hotelIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var hotel = _hotelService.Get(id);
+
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            _hotelService.Remove(hotel.Id);
+
+            return NoContent();
+        }
+    }
+}
--- HotelApp/Services/HotelService.cs
+++ HotelApp/Services/HotelService.cs
@@ -1,10 +1,42 @@
-﻿using System;
-namespace HotelApp.Services
+﻿using HotelsApi.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace HotelsApi.Services
 {
     public class HotelService
     {
-        public HotelService()
+        private readonly IMongoCollection<Hotel> _hotels;
+
+        public HotelService(IHotelDatabaseSettings settings)
         {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _hotels = database.GetCollection<Hotel>(settings.HotelsCollectionName);
         }
+
+        public List<Hotel> Get() =>
+            _hotels.Find(listing => true).ToList();
+
+        public Hotel Get(string id) =>
+            _hotels.Find<Hotel>(listing => listing.Id == id).FirstOrDefault();
+
+        public Hotel Create(Hotel hotel)
+        {
+            _hotels.InsertOne(hotel);
+            return hotel;
+        }
+
+        public void Update(string id, Hotel hotelIn) =>
+            _hotels.ReplaceOne(hotel => hotel.Id == id, hotelIn);
+
+        public void Remove(Hotel hotelIn) =>
+            _hotels.DeleteOne(hotel => hotel.Id == hotelIn.Id);
+
+        public void Remove(string id) =>
+            _hotels.DeleteOne(hotel => hotel.Id == id);
     }
 }
